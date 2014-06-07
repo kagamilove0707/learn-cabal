4d94f8fa593d6267365c1c7b3e25169ab6a0dd63
kagamilove0707 <kagamilove0707@gmail.com>
2014-06-07 15:36:33 +0900

    add doctest

--- Hello.hs
+++ Hello.hs
@@ -2,6 +2,10 @@ module Hello (
   hello
 ) where
 
--- | Say hello to `name`
+-- |
+-- Say hello to `name`
+--
+-- >>> hello "Haskell"
+-- "Hello Haskell World!"
 hello :: String -> String
 hello name = concat ["Hello ", name, " World!"]
